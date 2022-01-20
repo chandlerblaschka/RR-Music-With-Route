dd40b74081bd0601ac6bb7effee980fdbbf08176
chandlerblaschka <cblaschka@live.com>
2022-01-19 20:46:23 -0600

    code along no bonus

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,15 +1,48 @@
+import { render } from '@testing-library/react'
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 const AlbumView = () => {
     const { id } = useParams()
+    const [albumData, setAlbumData] = useState([])
+    const navigate = useNavigate()
 
-    const [albumData, setalbumData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/song/${id}`
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+            console.log(albumData)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.kind === 'song')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
+
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {navButtons()}
+            {albumData.length > 0 ? <h2>{albumData[0].collectionName}</h2> : <h2>Loading...</h2>}
+            {renderSongs}
         </div>
     )
 }
